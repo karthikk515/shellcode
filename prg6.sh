92fc1576fd4f15004c2ba4849dd6763e0609b461
Karthik Kondapaneni <153291554+karthikk515@users.noreply.github.com>
2024-02-11 18:11:20 +0530

    Update prg6.sh

--- prg6.sh
+++ prg6.sh
@@ -5,4 +5,5 @@ echo "My LogName is`logname`"
 #or you can use below statement
 echo "My login name is $(logname)"
 echo " I am using $(uname) Operating System"
+echo "Im the Owner on $(hostname) Sever"
 #End of the Program
